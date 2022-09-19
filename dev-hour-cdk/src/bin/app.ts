34930059ad8c4ee9ccdd09ac690bdb29aab96173
Carlos Cuenca <cuenca@Carloss-MacBook-Pro.local>
2022-09-19 05:04:29 -0700

    Add preliminary cdk

---
+++ dev-hour-cdk/src/bin/app.ts
@@ -0,0 +1,34 @@
+/**
+ * App entrypoint.
+ * @version 0.1.0
+ */
+
+import { App } from '@aws-cdk/core'
+import { ShellStep } from '@aws-cdk/pipelines'
+import { PipelineStack, PipelineStackProps } from '../lib/pipeline-stack'
+import { AlphaStage } from '../lib/alpha-stage'
+import { Constants } from '../lib/constants'
+
+const app = new App();
+
+const stages = [
+    new AlphaStage(app, {
+        account:   Constants.Account,
+        region:    Constants.Region,
+        stageId:   Constants.Stages.Alpha.Id,
+        stageName: Constants.Stages.Alpha.Name
+    })
+];
+
+new PipelineStack(app, {
+    account:    Constants.Account,
+    region:     Constants.Region,
+    stackId:    Constants.CodePipeline.StackId,
+    pipelineId: Constants.CodePipeline.Id,
+    shellStep:  new ShellStep(Constants.CodePipeline.ShellStep.Id, {
+        commands: []
+    }),
+    stages:     stages
+});
+
+app.synth()
