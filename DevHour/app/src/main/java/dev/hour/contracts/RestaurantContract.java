ac133d6c33951cf6a340a7e076e6590c0c432359
patrick trinidad <ptrinidad062@gmail.com>
2022-09-18 20:49:16 -0700

    Im running out of names

--- DevHour/app/src/main/java/dev/hour/contracts/RestaurantContract.java
+++ DevHour/app/src/main/java/dev/hour/contracts/RestaurantContract.java
@@ -1,5 +1,7 @@
 package dev.hour.contracts;
 
+import java.util.List;
+
 public interface RestaurantContract {
     
     public interface Database {
@@ -14,10 +16,12 @@ public interface RestaurantContract {
         void setDatabase(Database d);
         void setView(View v);
 
+        void invalidate();
     }
 
     public interface View {
 
+        void setRestaurants(List <Restaurant> restaurants);
     }
 
     public interface Restaurant {
@@ -25,12 +29,22 @@ public interface RestaurantContract {
         String getName();
         double getLongitude();
         double getLatitude();
+        int getPricing();
+        List getMeals();
 
-    }
+        }
     public interface Meal {
 
         int getCalories();
+        String getName();
+        List getIngredients();
 
+    }
+
+    public interface Ingredient{
+        int getCalories();
+        String getName();
+        
     }
 
 }
