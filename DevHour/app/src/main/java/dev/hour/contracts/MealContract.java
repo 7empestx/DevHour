178584992e8db1adfd8489176accf08265024e61
Carlos Cuenca <cuenca@Carloss-MacBook-Pro.local>
2022-09-21 21:17:36 -0700

    Update Contracts

--- DevHour/app/src/main/java/dev/hour/contracts/MealContract.java
+++ DevHour/app/src/main/java/dev/hour/contracts/MealContract.java
@@ -1,6 +1,7 @@
 package dev.hour.contracts;
 
 import java.util.List;
+import java.util.Map;
 
 public interface MealContract {
 
@@ -8,16 +9,62 @@ public interface MealContract {
 
         int getCalories();
         String getName();
-        List<Diet> getDiets();
-        List<Ingredient> getIngredients();
-
-    }
-
-    enum Diet {
+        Map<String, Ingredient> getIngredients();
 
     }
 
     enum Ingredient {
+
+    }
+
+    interface Diet {
+
+        List<Ingredient> getAllergens();
+
+        interface Database {
+
+            Diet getDiet(final String id);
+
+        }
+
+        interface Presenter {
+
+            void setDatabase(final Database database);
+            void setView(final View view);
+
+        }
+
+        interface View {
+
+            void setDiet(final Diet diet);
+
+        }
+
+    }
+
+    interface Menu {
+
+        String getName();
+        List<Meal> getMeals();
+
+        interface Database {
+
+            List<Meal> getMenu();
+
+        }
+
+        interface Presenter {
+
+            void setDatabase(final Database database);
+            void setView(final View view);
+
+        }
+
+        interface View {
+
+            void setMenu(final Menu menu);
+
+        }
 
     }
 
