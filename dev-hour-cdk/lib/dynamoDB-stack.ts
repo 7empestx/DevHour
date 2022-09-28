10d5164a32ec9a25e75722f032b570cdb3444a20
7empestx <grant.starkman@gmail.com>
2022-09-28 13:27:12 -0700

    imports and exports

--- dev-hour-cdk/lib/dynamoDB-stack.ts
+++ dev-hour-cdk/lib/dynamoDB-stack.ts
@@ -1,8 +1,6 @@
+import {} from 'aws-cdk-lib/aws-dynamodb'
 import { Stack } from 'aws-cdk-lib'
-import { dynamodb } from 'aws-cdk-lib/aws-dynamodb'
 
-export class DynamoDBStack extends Stack {
-    const table = new dynamodb.Table(this, 'Table', {
-        partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
-    });
+export class dynamoDBStack extends Stack {
+  
 }
