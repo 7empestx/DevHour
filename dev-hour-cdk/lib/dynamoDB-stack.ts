07c282eae8cf9a7563eed96cb4171f129b7faa2c
7empestx <grant.starkman@gmail.com>
2022-09-28 22:25:59 -0700

    db work

--- dev-hour-cdk/lib/dynamoDB-stack.ts
+++ dev-hour-cdk/lib/dynamoDB-stack.ts
@@ -1,6 +1,20 @@
-import {} from 'aws-cdk-lib/aws-dynamodb'
+import { Table } from 'aws-cdk-lib/aws-dynamodb'
 import { Stack } from 'aws-cdk-lib'
+import { Construct } from 'constructs'
+
+export interface TableProps {
+
+}
 
 export class dynamoDBStack extends Stack {
-  
+    private readonly _db: Table;
+    
+    constructor(scope: Construct, id: string, props: TableProps) { 
+        super();
+   }
+
+   public get table() {
+        return this._db;
+    }
 }
+
