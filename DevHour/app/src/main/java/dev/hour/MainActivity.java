bb301ff8fcbf1c7ec6f4dc01fa9057b9c0b5b9ce
Carlos Cuenca <cuenca@Carloss-MacBook-Pro.local>
2022-10-03 10:52:56 -0700

    More Fixing

--- DevHour/app/src/main/java/dev/hour/MainActivity.java
+++ DevHour/app/src/main/java/dev/hour/MainActivity.java
@@ -9,27 +9,6 @@ import dev.hour.contracts.AuthenticatorContract;
 
 public class MainActivity extends AppCompatActivity {
 
-    private AuthenticatorContract.Authenticator authenticator;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        authenticator = new Authenticator(this);
-
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-        if(authenticator != null) {
-
-            authenticator.checkSession();
-
-        }
-
-    }
 
 }
