0b9eb5b8188b1badfbe9ce23a3cac03ee02a740d
Esteban Rivas Rey <81936875+EstRR@users.noreply.github.com>
2022-10-04 21:14:40 -0700

    created constructor

--- DevHour/app/src/main/java/dev/hour/database/RestaurantDatabase.java
+++ DevHour/app/src/main/java/dev/hour/database/RestaurantDatabase.java
@@ -6,6 +6,21 @@ import dev.hour.contracts.RestaurantContract;
 
 public class RestaurantDatabase implements RestaurantContract.Database {
 
+    private DynamoDbClient client;
+    private String tableName;
+
+    public RestaurantDatabase(final String regionName, final String tableName){
+
+        // Create client
+        client = DynamoDbClient
+                .builder()
+                .region(Region.of(regionName))
+                .build();
+
+        // Set table name
+        this.tableName = tableName;
+    }
+
     @Override
     public RestaurantContract.Restaurant getRestaurant(final String id) {
         return null;
