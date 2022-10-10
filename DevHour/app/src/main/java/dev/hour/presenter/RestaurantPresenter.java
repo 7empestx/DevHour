52b7198036c6eae432d60a8d61797b907c65459a
Daniel Pintado <Pintadodaniel@protonmail.com>
2022-10-09 19:27:32 -0700

    update with actual code lol

--- DevHour/app/src/main/java/dev/hour/presenter/RestaurantPresenter.java
+++ DevHour/app/src/main/java/dev/hour/presenter/RestaurantPresenter.java
@@ -1,2 +1,30 @@
-package dev.hour.presenter;public class RestaurantPresenter {
+package dev.hour.presenter;
+
+import java.util.List;
+
+import dev.hour.contracts.RestaurantContract;
+
+public class RestaurantPresenter implements RestaurantContract.Presenter {
+
+    //Private Members
+    private RestaurantContract.Database database;
+    private RestaurantContract.View view;
+    private List<RestaurantContract.Restaurant> resturants; //this is missing in the UML..?
+
+    @Override
+    public void setDatabase(RestaurantContract.Database database) {
+        this.database = database;
+    }
+
+    @Override
+    public void setView(RestaurantContract.View view) {
+
+    }
+
+    @Override
+    public void invalidate() {
+        if(view != null) {
+            view.setRestaurants(resturants);
+        }
+    }
 }
