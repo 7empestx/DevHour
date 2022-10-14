3ba45764c06907daeadbaddf658106e9ba7f295c
sinceredevotion <bibik.yuriy.by@gmail.com>
2022-10-14 00:01:45 -0700

    Initial Search Bar Implementation

---
+++ DevHour/app/src/main/java/dev/hour/view/SearchBar.java
@@ -0,0 +1,19 @@
+package dev.hour.view;
+
+import static android.view.ViewGroup.LayoutParams.WRAP_CONTENT;
+
+import android.content.Context;
+import android.util.DisplayMetrics;
+
+import androidx.appcompat.widget.SearchView;
+
+public class SearchBar extends SearchView
+{
+
+
+    public SearchBar(Context context)
+    {
+        super(context);
+    }
+
+}
