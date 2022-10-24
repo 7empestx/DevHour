38a092285af6a53448a0b6301a5f34897a272640
mata-m <m.mata3698@gmail.com>
2022-10-24 14:05:14 -0700

    Implemented Meal interface

---
+++ DevHour/app/src/main/java/dev/hour/meal/Meal.java
@@ -0,0 +1,32 @@
+package dev.hour.meal;
+
+import java.time.chrono.MinguoDate;
+import java.util.Map;
+
+import dev.hour.contracts.MealContract;
+
+public class Meal implements MealContract.Meal {
+    /// --------------
+    /// Private Fields
+
+    private int calories;
+    private String name;
+    private Map<String, MealContract.Ingredient> ingredients;
+
+    public Meal(final String name, final int calories, final Map<String, MealContract.Ingredient> ingredients) {
+
+        this.name = name;
+        this.calories = calories;
+        this.ingredients = ingredients;
+
+    }
+
+    @Override
+    public int getCalories() { return calories; }
+
+    @Override
+    public String getName() { return name; }
+
+    @Override
+    public Map<String, MealContract.Ingredient> getIngredients() { return ingredients; }
+}
