ca8f8ba29f55f474aec39c0d92b7e1104880dbcd
7empest <51809789+7empestx@users.noreply.github.com>
2022-11-15 22:50:12 -0800

    business fragments

---
+++ DevHour/app/src/main/java/dev/hour/fragment/BusinessMenuItemDetail.java
@@ -0,0 +1,20 @@
+package dev.hour.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import dev.hour.R;
+
+public class BusinessMenuItemDetail extends Fragment {
+    public final static String TAG = "BusinessMenuItemDetail";
+
+    @Override
+    public View onCreateView(LayoutInflater lf, ViewGroup vg, Bundle b){
+        final View layout = (View) lf.inflate(R.layout.fragment_business_menu_item_detail, vg, false);
+        return layout;
+    }
+}
