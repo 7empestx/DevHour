f503205999aef079331ac6f9ed3a3efcd4ba1a4c
priment <kevint.sweitzer@gmail.com>
2022-11-15 19:27:15 -0800

    innit commit  of add restaurant screen

---
+++ DevHour/app/src/main/java/dev/hour/view/list/AddRestaurantListAdapter.java
@@ -0,0 +1,26 @@
+package dev.hour.view.list;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class AddRestaurantListAdapter extends RecyclerView.Adapter<RestaurantListAdapter.RestaurantListItemViewHolder> {
+
+
+    @NonNull
+    @Override
+    public RestaurantListAdapter.RestaurantListItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RestaurantListAdapter.RestaurantListItemViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
