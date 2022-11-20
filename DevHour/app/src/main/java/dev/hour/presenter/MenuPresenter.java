b13de156323a31b39a16ceb63a6f0f4f87d4116e
7empest <51809789+7empestx@users.noreply.github.com>
2022-11-20 00:01:50 -0800

    businessMenuListImplmentations

--- DevHour/app/src/main/java/dev/hour/presenter/MenuPresenter.java
+++ DevHour/app/src/main/java/dev/hour/presenter/MenuPresenter.java
@@ -1,11 +1,15 @@
 package dev.hour.presenter;
 
+import java.util.List;
+
 import dev.hour.contracts.MealContract;
+import dev.hour.contracts.RestaurantContract;
 
 public class MenuPresenter implements MealContract.Menu.Presenter {
     //Private Members
     private MealContract.Menu.Database database;
     private MealContract.Menu.View view;
+    private List<MealContract.Meal> meals ;
 
     @Override
     public void setDatabase(MealContract.Menu.Database database) {
@@ -19,7 +23,11 @@ public class MenuPresenter implements MealContract.Menu.Presenter {
 
     @Override
     public void setView(MealContract.Menu.View view) {
+        // Set the view
+        this.view = view;
 
+        if(this.view != null)
+            this.view.setMenu(meals);
     }
 }
 
