8bc1063f5ca58beaeb413ef3bc700217939cc870
mata-m <m.mata3698@gmail.com>
2022-11-20 18:01:23 -0800

    Added Search Box and List View for user dietary preferences. Implemented List View filtering by search. Removed old switch interface for dietary preferences. Missing updating database table when list item is un/checked.

--- DevHour/app/src/main/java/dev/hour/MainActivity.java
+++ DevHour/app/src/main/java/dev/hour/MainActivity.java
@@ -737,6 +737,8 @@ public class MainActivity extends AppCompatActivity implements
             fragment = new ProfileFragment();
             ((UserContract.View)fragment)
                     .setUserListener((UserContract.View.Listener) userPresenter);
+            ((MealContract.Diet.View)fragment)
+                    .setDietListener((MealContract.Diet.View.Listener) dietPresenter);
             userPresenter.setView((UserContract.View) fragment);
 
             dietPresenter.setView((MealContract.Diet.View) fragment);
@@ -746,8 +748,11 @@ public class MainActivity extends AppCompatActivity implements
         } else if(fragment.isAdded()) {
             ((UserContract.View)fragment)
                     .setUserListener((UserContract.View.Listener) userPresenter);
+            ((MealContract.Diet.View)fragment)
+                    .setDietListener((MealContract.Diet.View.Listener) dietPresenter);
 
             userPresenter.setView((UserContract.View) fragment);
+            dietPresenter.setView((MealContract.Diet.View) fragment);
 
         }
 
